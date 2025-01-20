19220c68f89f782975ed4838cb73599b521d5961
jacob-53 <jacob8753@gmail.com>
2025-01-20 23:36:23 +0900

    modify feature

--- src/cob_eda/cli.py
+++ src/cob_eda/cli.py
@@ -1,28 +1,25 @@
 from president_speech.db.parquet_interpreter import read_parquet, get_parquet_full_path
 import pandas as pd
-import typer
 
-app = typer.Typer()
 
-@app.command()
-
-def psearch_by_count(keyword: str ):
+def psearch_by_count():
     data_path = get_parquet_full_path()
     df = pd.read_parquet(data_path)
-    f_df = df[df['speech_text'].str.contains(str(keyword), case=False)]
+    
     while True:
+        keyword = input ("검색할 키워드를 입력하세요(종료를 원하면 '종료하겠습니다'를 입력하세요)")
+        if keyword == '종료하겠습니다':
+            print ("종료합니다")
+            break
+        f_df = df[df['speech_text'].str.contains(str(keyword), case=False)]
         if not f_df.empty:
-            pass
+            rdf = f_df.groupby("president").size().reset_index(name="count").sort_values(by="count", ascending=False)
+            sdf = rdf.sort_values(by='count', ascending=False).reset_index(drop=True)
+            print(sdf.to_string(index=False))
+        
         else:
-            typer.echo(f" '{keyword}'은 언급 되지 않았습니다.")
-            keyword = typer.prompt("새로운 키워드를 입력하세요")
-    rdf = f_df.groupby("president").size().reset_index(name="count").sort_values(by="count", ascending=False)
-    sdf = rdf.sort_values(by='count', ascending=False).reset_index(drop=True)
-    print(sdf.to_string(index=False))
-
-if __name__ == "__main__":
-    app()
-
+            print("일치하는 값이 없습니다")
+            continue 
 
 def group_by_count():
     kw = "올림픽"
